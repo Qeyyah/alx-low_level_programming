ed1e33375907f32a68e431f5a7ce58268387448e
Qeyyah <rukayat.o.akano@gmail.com>
2022-09-09 14:05:10 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
 * main - print if the number is positive, zero, or negative
 *
-* Description 
+* Description: using the main function 
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
@@ -15,5 +15,5 @@ for (i = 0; i < 10; i++)
 	printf("%i", i);
 }
 printf("\n");
-return (0)'
+return (0);
 }
