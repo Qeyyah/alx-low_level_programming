2192b8fce21b6584d4b5f45f36a3db4f890adf86
Qeyyah <rukayat.o.akano@gmail.com>
2022-09-13 11:57:40 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 
 #include "main.h"
 /**
-* print_alphabet_*10 - function to print abc 10 times
+* print_alphabet_x10 - function to print abc 10 times
 *
 * Return: 0
 */
-void print_alphabet_*10(void)
+void print_alphabet_x10(void)
 {
 	char c, i,
 
