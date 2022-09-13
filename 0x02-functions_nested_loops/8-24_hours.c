0b61d8ddca2e95055b17467f49a2e5430075c305
Qeyyah <rukayat.o.akano@gmail.com>
2022-09-13 14:40:31 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,9 +21,9 @@ void jack_bauer(void)
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
-	_putchar(';');
+	_putchar(':');
 	_putchar(minutes / 10 + '0');
-	_putchar(mins_reminder + '0');
+	_putchar(mins_remainder + '0');
 	minutes++;
 	_putchar('\n');
 	}
