5ee0c4bd8762916fd6dacaadde6d6e2b16b8e2a2
Qeyyah <rukayat.o.akano@gmail.com>
2022-09-13 16:54:57 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,6 @@
 
 #include "main.h"
+
 /**
  * jack_bouer - func that prints every minute of the day of Jack Bouer, starti
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
